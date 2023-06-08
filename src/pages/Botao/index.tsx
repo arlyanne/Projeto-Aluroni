a0bce63754b522b263cf60320a1813e72cd059d2
Arlyanne <arlyanne.lima.ferro@gmail.com>
2023-06-08 19:58:36 -0300

    Importar fontes externas;Utilizar o normalize.css;Utilizar icones;Criar variaveis css

--- src/pages/Botao/index.tsx
+++
@@ -1,9 +0,0 @@
-import Styles from './Botao.module.scss';
-
-export default function Botao() {
-    return(
-        <button className={Styles.Botao}>
-            Click aqui!
-        </button>
-    );
-}

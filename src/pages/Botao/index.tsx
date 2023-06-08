06efda8e4c9df5fcc3d19122f57951887365a9a0
Arlyanne <arlyanne.lima.ferro@gmail.com>
2023-06-08 15:32:20 -0300

    AUlas com React - TypeScript e JavaScript

---
+++ src/pages/Botao/index.tsx
@@ -0,0 +1,9 @@
+import Styles from './Botao.module.scss';
+
+export default function Botao() {
+    return(
+        <button className={Styles.Botao}>
+            Click aqui!
+        </button>
+    );
+}

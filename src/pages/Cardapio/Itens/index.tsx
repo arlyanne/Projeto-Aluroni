1a6db0bbe17ecb36d675b898203bbfb72e1a9e92
Arlyanne <arlyanne.lima.ferro@gmail.com>
2023-06-14 15:41:55 -0300

    Renderizando componentes; Aprendendo a utilizar a pasta public; Gerando arquivos utilizados em produçao

---
+++ src/pages/Cardapio/Itens/index.tsx
@@ -0,0 +1,16 @@
+import Item from './Item';
+import cardapio from './intens.json';
+import styles from './Itens.module.scss';
+
+export default function Itens() {
+    return (
+        <div className={styles.itens}>
+           {cardapio.map((item) => (
+            <Item 
+              key={item.id} 
+              {...item}
+            />
+           ))}
+        </div>
+    )
+}
